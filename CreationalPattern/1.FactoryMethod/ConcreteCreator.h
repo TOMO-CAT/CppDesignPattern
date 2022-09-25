df1c9f9cc0e025d8cd8350569020cc28b9e9de01
SearchAdDeveloper <SearchAdDeveloper@qq.com>
2022-09-25 19:45:37 +0800

    fix: factory method

--- CreationalPattern/1.FactoryMethod/ConcreteCreator.h
+++ CreationalPattern/1.FactoryMethod/ConcreteCreator.h
@@ -1,32 +1,26 @@
 #ifndef CONCRETE_CREATOR_H_
 #define CONCRETE_CREATOR_H_
 
-#include <memory>
-#include "Creator.h"
 #include "ConcreteProduct.h"
+#include "Creator.h"
+#include <memory>
 
 // 具体工厂类 中国生产者
 class ChineseProducer : public Factory {
  public:
-    std::shared_ptr<Movie> get_movie() override {
-        return std::make_shared<ChineseMovie>();
-    }
+    std::shared_ptr<Movie> get_movie() override { return std::make_shared<ChineseMovie>(); }
 };
 
 // 具体工厂类 日本生产者
 class JapaneseProducer : public Factory {
  public:
-    std::shared_ptr<Movie> get_movie() override {
-        return std::make_shared<ChineseMovie>();
-    }
+    std::shared_ptr<Movie> get_movie() override { return std::make_shared<JapaneseMovie>(); }
 };
 
 // 具体工厂类 美国生产者
 class AmericanProducer : public Factory {
  public:
-    std::shared_ptr<Movie> get_movie() override {
-        return std::make_shared<ChineseMovie>();
-    }
+    std::shared_ptr<Movie> get_movie() override { return std::make_shared<AmericanMovie>(); }
 };
 
 #endif  // CONCRETE_CREATOR_H_
